f4a633e8d2369a08489c0a805725ee1d7e58f8aa
Mike Qin <1mike12@gmail.com>
2025-04-24 16:13:02 -0600

    improve LRU tests coverage

--- src/data_structures/LRU/LRU.test.ts
+++ src/data_structures/LRU/LRU.test.ts
@@ -1,5 +1,5 @@
-import {expect} from "chai";
-import {LRU} from "./LRU";
+import { expect } from "chai";
+import { LRU } from "./LRU";
 
 describe("LRU", () => {
    it("should bump older values", () => {
@@ -46,4 +46,45 @@ describe("LRU", () => {
       expect(lru.get("a")).to.equal(1);
       expect(lru.get("c")).to.equal(3);
    })
+
+   it("should resize the cache", () => {
+      const lru = new LRU(2);
+      lru.set("a", 1);
+      lru.set("b", 2);
+      lru.resize(1);
+      expect(lru.get("a")).to.equal(undefined);
+   })
+
+   it("should be able to iterate over the cache", () => {
+      const lru = new LRU<string, number>(2);
+      lru.set("a", 1);
+      lru.set("b", 2);
+      const result: [string, number][] = [];
+      for (const [key, value] of lru) {
+         result.push([key, value]);
+      }
+      expect(result).to.deep.equal([["a", 1], ["b", 2]]);
+   })
+
+   it("should be able to iterate over the cache keys", () => {
+      const lru = new LRU<string, number>(2);
+      lru.set("a", 1);
+      lru.set("b", 2);
+      const result: string[] = [];
+      for (const key of lru.keys()) {
+         result.push(key);
+      }
+      expect(result).to.deep.equal(["a", "b"]);
+   })
+
+   it("should be able to iterate over the cache values", () => {
+      const lru = new LRU<string, number>(2);
+      lru.set("a", 1);
+      lru.set("b", 2);
+      const result: number[] = [];
+      for (const value of lru.values()) {
+         result.push(value);
+      }
+      expect(result).to.deep.equal([1, 2]);
+   });
 });
