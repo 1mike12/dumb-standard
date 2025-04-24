51ae3c4cc44fc35827bd0ca32809a68fb0e27a8a
Mike Qin <1mike12@gmail.com>
2025-04-24 11:41:45 -0600

    fix mode test

--- src/math/central_tendency/mode.test.ts
+++ src/math/central_tendency/mode.test.ts
@@ -3,14 +3,14 @@ import { mode } from "./mode";
 
 describe("mode", () => {
   it("should return the mode of an array of numbers", () => {
-    expect(mode([1, 2, 3])).to.equal([1]);
+    expect(mode([1, 2, 3])).to.deep.equal([1, 2, 3]);
   });
 
   it("should return the mode of an array of numbers with multiple modes", () => {
-    expect(mode([1, 2, 2, 3, 3])).to.equal([2, 3]);
+    expect(mode([1, 2, 2, 3, 3])).to.deep.equal([2, 3]);
   });
 
   it("should return the mode of an array of numbers with a single mode", () => {
-    expect(mode([1, 2, 2, 3, 3, 3])).to.equal([3]);
+    expect(mode([1, 2, 2, 3, 3, 3])).to.deep.equal([3]);
   });
 });
